265eee6557d67db07dc8871ba14ba3fc01d71bed
riera90@alpha <diegorodriguezriera@gmail.com>
2018-03-22 20:43:45 +0100

    added automatic screen detection

--- bgd.sh
+++ bgd.sh
@@ -1,11 +1,16 @@
 #/bin/zsh
+printf "\n"
 top_value=10
 secundary_screen_brightnes_offset=1
-brightness_main=$(calc $1 / 10)
-brightness_secundary=$(calc $(calc $1 / $top_value) + $(calc $secundary_screen_offset / $top_value ))
-main_screen=LVDS-1
-Secundary_screen=VGA-1-2
+brightness=$(calc $1 / 10)
 debug=0 # [1/0]
+
+screens=$(xrandr | sed -rn 's/^([A-Z0-9\-]*)\ connected.*/\1/p')
+printf "\n Detected screens: \n"
+for screen in $screens; do
+	printf "\t$screen\n"
+done
+# exit
 
 if [[ ${#} -lt 1 ]]; then
 	echo "exit!"
@@ -16,7 +21,7 @@ if [[ "$1" -lt "3" ]]; then
 	printf "TO LOW!! \nUnless you want to have a nice time trying to get it back...\n"
 	exit
 fi
-
+printf "\n Gloval configuration:\n"
 if [[ ${#} -gt "3" ]]; then
 	printf "\tSetting custom filter..."
 	r=$(clac $2 / $top_value)
@@ -43,6 +48,7 @@ else
 		fi
 	else
 		# echo "default rgb"
+		printf "\tNothing to make.\n"
 		r=1
 	  g=1
 	  b=1
@@ -54,17 +60,17 @@ fi
 # exit
 
 # if [[ "$brightness" == 10 ]]; then
+for screen in $screens; do
+	printf "\n Changing $screen:\n"
 	printf "\tapl changes..."
-	xrandr --output "$main_screen" --brightness "$brightness_main" --gamma "$r":"$g":"$b"
-	xrandr --output "$Secundary_screen" --brightness "$brightness_secundary" --gamma "$r":"$g":"$b"
+	xrandr --output "$screen" --brightness "$brightness" --gamma "$r":"$g":"$b"
 	if [[ "$debug" == "1" ]]; then
-		echo "default brightness"
-		printf "\n"
-		echo -e "xrandr --output "$main_screen" --brightness "$brightness_main" --gamma "$r":"$g":"$b""
-		echo -e "xrandr --output "$Secundary_screen"1 --brightness "$brightness_secundary" --gamma "$r":"$g":"$b""
+		printf "\nDEBUG: default brightness\n"
+		printf "DEBUG: xrandr --output "$screen" --brightness "$brightness" --gamma "$r":"$g":"$b""
 	fi
 	printf " done.\n"
-
+done
+printf "\n exito\n\n"
 # else
 #   echo "added brightness"
 # 	xrandr --output "$main_screen" --brightness "$brightness_main" --gamma "$r":"$g":"$b"
