3c5c80757062f26e8ed5cdca8425ba71627aeb72
riera90 <diegorodriguezriera@gmail.com>
2018-05-22 21:42:20 +0200

    added cache_clear script

---
+++ clear_cache.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+free
+sync
+sudo swapoff -a
+sudo sh -c 'echo 1 >/proc/sys/vm/drop_caches'
+sudo sh -c 'echo 2 >/proc/sys/vm/drop_caches'
+sudo sh -c 'echo 3 >/proc/sys/vm/drop_caches'
+sudo swapon -a
+free
