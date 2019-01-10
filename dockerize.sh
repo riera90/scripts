9f493e8da95dd5b3b65d815815f88bb9c20ba2ae
diego@alpha <diegorodriguezriera@gmail.com>
2019-01-10 18:12:57 +0100

    added symfony password tester

--- dockerize.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-echo "running docker daemon"
-sudo systemctl start docker.socket
-echo "launching docker"
-sudo docker run --name=SynfonyApplication --rm -it --net=host -v $(pwd):/application riera90/symfony-dev
